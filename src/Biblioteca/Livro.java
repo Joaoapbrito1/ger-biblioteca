e9727b18db9dd13f9ff8cf3cd90e50de5d019616
Joaoapbrito1 <joao.brito@zup.com.br>
2024-12-20 19:53:11 -0300

    feat: implement the attributes and constructor of the book class.

--- src/Biblioteca/Livro.java
+++ src/Biblioteca/Livro.java
@@ -1,4 +1,19 @@
 package Biblioteca;
 
-public class Livro {
+import java.io.Serializable;
+
+public class Livro implements Serializable {
+    //Atributos
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private boolean disponivel;
+
+    //Construtor
+    public Livro(String titulo, String autor, String isbn) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.disponivel = true;
+    }
 }
